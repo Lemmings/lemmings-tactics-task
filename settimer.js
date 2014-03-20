f886849110c4c172ac20f54502d8645868da7bfd
you21979 <you2197901@gmail.com>
2014-03-20 15:16:52 +0900

    initial commit

---
+++ settimer.js
@@ -0,0 +1,7 @@
+exports.initialize = function(){};
+exports.finalize = function(){};
+exports.run = function(inputTools, callback){
+    var args = inputTools.args();
+    inputTools.timer = args.timer;
+    callback(null);
+};
---
+++ void.js
@@ -0,0 +1,5 @@
+exports.initialize = function(){};
+exports.finalize = function(){};
+exports.run = function(inputTools, callback){
+    callback(null);
+};
